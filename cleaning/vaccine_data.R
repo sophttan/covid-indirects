8f2d1201727581c0d5671e97c3f2402b847a9758
sophttan <sophiatan@berkeley.edu>
2022-04-12 13:24:42 -0700

    remove duplicate vaccine records (different vaccine types in 1 day)

--- cleaning/vaccine_data.R
+++ cleaning/vaccine_data.R
@@ -22,9 +22,14 @@ covid_vacc <- covid_vacc %>% mutate(Date = as.Date(Date),
 summary_resident_data <- covid_vacc %>% filter(Result=="Received") %>% group_by(ResidentId) %>% summarise(num_doses=n(), num_types=length(unique(Vaccine))) %>%
   filter(num_types != 1)
 
-covid_vacc_cleaned <- covid_vacc %>% filter(Result == "Received") %>% arrange(Date) %>% 
+# there are 50 residents with multiple records of vaccination on a single day (i.e. 1 dose of J&J and 1 dose of Moderna vaccine on 6/5/21)
+# collapse records but unsure which vaccine type is correct
+covid_vacc_cleaned <- covid_vacc %>% filter(Result=="Received") %>% group_by(ResidentId, Date) %>% summarise(Vaccine=paste(Vaccine, collapse = "|"), 
+                                                                                                            Month=first(Month))
+covid_vacc_cleaned <- covid_vacc_cleaned %>% arrange(Date) %>% 
   group_by(ResidentId) %>% mutate(num_dose = 1:n(), max_dose = n()) %>% arrange(ResidentId)
 
+# if someone was marked to have received any mrna vaccine as their primary series (even if both j&j and mrna reported), mark as mrna vacc
 covid_vacc_cleaned <- covid_vacc_cleaned %>% 
   mutate(full_vacc = ifelse(grepl("mRNA", Vaccine[1]), 2, 1),
          booster_add_dose = ifelse(max(num_dose) > 2 | (grepl("Ad26", Vaccine[1]) & max(num_dose > 1)), 1, 0),
@@ -34,7 +39,7 @@ covid_vacc_cleaned <- covid_vacc_cleaned %>%
 # 99128 residents received at least 1 dose of a vaccine
 num_res_vacc <- covid_vacc_cleaned$ResidentId %>% unique() %>% length()
 
-# 3332 residents (3% of vaccinated residents) received only 1 dose of an mRNA vaccine
+# 3336 residents (3% of vaccinated residents) received only 1 dose of an mRNA vaccine
 covid_vacc_cleaned %>% filter(incomplete==1)
 
 # 38516 residents (38% of vaccinated residents) have completed series but no booster (not accounting for time eligibility)
@@ -49,4 +54,4 @@ boosted <- covid_vacc_cleaned %>% filter(booster_add_dose==1)
 (boosted %>% filter(booster_add_dose_mixed==1) %>% filter(num_dose==1))$Vaccine %>% table()
 
 
-write_csv(covid_vacc_cleaned, "D:/code_ST/cleaned_vaccination_data.csv")
+write_csv(covid_vacc_cleaned, "D:/stan5/code_ST/cleaned_vaccination_data.csv")
