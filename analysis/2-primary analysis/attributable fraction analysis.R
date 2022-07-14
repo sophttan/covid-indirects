4ecbedb5ed959841cd0d90c966c383d687c1a24b
Sophia Tan <sophiatan@berkeley.edu>
2022-07-14 13:35:11 -0700

    attributable fraction

--- analysis/2-primary analysis/attributable fraction analysis.R
+++ analysis/2-primary analysis/attributable fraction analysis.R
@@ -27,42 +27,87 @@ binomial <- function(data) {
     unnest(out) %>% select(!c(statistic, p.value, parameter, method, alternative))
 }
 
-transmission_frac <- d %>% group_by(index_prior_vacc, index_prior_inf) %>% 
+transmission_frac <- d %>% group_by(index_prior_inf,index_prior_vacc) %>% 
   summarise(prim_cases=n(), total_prim_cases=1261, secondary_cases=sum(contact_status), secondary_total=d$contact_status %>% sum()) %>% 
-  arrange(index_prior_vacc, index_prior_inf) %>% ungroup() %>% 
+  ungroup() %>% 
   mutate(prop_primary = prim_cases/total_prim_cases*100, 
          prop_secondary = secondary_cases/secondary_total*100) %>% binomial() %>% select(!c(3:6))
 
 
-# attributable fraction among 
-d <- read_csv("housing_inf_data.csv")
-d %>% group_by(ResidentId) %>% filter(any(!Institution %>% is.na())|any(!RoomId %>% is.na()))
-omicron <- d %>% group_by(ResidentId, num_pos) %>% filter(num_pos>0) %>% filter(first(Day)>="2021-12-15")
+
+d <- d %>% mutate(Institution=as.factor(Institution),
+                  index_id=as.factor(index_id))
+
+# pre-specified model
+model <- glm(contact_status ~ index_prior_vacc + index_prior_inf +
+               num_vacc_doses+has_prior_inf+incidence_log+Institution, data=d, weights=weights, family="poisson")
+# find SE of linear combination of covariates (prior vaccination and prior infection)
+error <- c(sqrt(t(c(0,1,1,rep(0,28))) %*% vcovCR(model, cluster = d$subclass, type="CR2") %*% c(0,1,1,rep(0,28))))
+model <- coef_test(model, vcov = "CR2", cluster = d$subclass) %>% data.frame(row.names = NULL)
+main_results <- model[2:3,] %>% select(Coef, beta, SE) %>% 
+  rbind(list("both_vacc_inf", sum(.$beta), error))
+
+est_relative_risk <- function(data) {
+  data %>% mutate(risk_red = exp(beta),
+                  lb = exp(beta-2*SE),
+                  ub = exp(beta+2*SE))
+}
+
+main_results <- main_results %>% est_relative_risk()
+main_results
+
+# attributable fraction among all omicron infections
+full <- read_csv("housing_inf_data.csv")
+full %>% group_by(ResidentId) %>% filter(any(!Institution %>% is.na())|any(!RoomId %>% is.na()))
+omicron <- full %>% group_by(ResidentId, num_pos) %>% filter(num_pos>0) %>% filter(first(Day)>="2021-12-15")
 omicron <- omicron %>% summarise_all(first)
+
 summary_omicron <- omicron %>% mutate(prior_inf = ifelse(num_pos>1, 1, 0), 
-                   prior_vacc = ifelse(num_dose_adjusted>0, 1, 0)) %>% group_by(prior_vacc, prior_inf) %>% summarise(count=n())
-est_transmission <- summary_omicron$count * (exp(-1.34337)*c(1, 1+(main_results$est[c(2,1,6)])/100))
-est_transmission/sum(est_transmission)*100
+                   prior_vacc = ifelse(num_dose_adjusted>0, 1, 0)) %>%
+  group_by(prior_inf,prior_vacc) %>% summarise(count=n())
+est_transmission <- summary_omicron$count*(exp(model[1,]$beta)*c(1, main_results$risk_red))
+est <- est_transmission/sum(est_transmission)*100
 
-est_transmission <- summary_omicron$count * (exp(-1.34337)*c(1, 1+(main_results$lb[c(2,1,6)])/100))
-est_transmission/sum(est_transmission)*100
+est_transmission <- summary_omicron$count*(exp(model[1,]$beta)*c(1, main_results$lb))
+lb <- est_transmission/sum(est_transmission)*100
 
-est_transmission <- summary_omicron$count * (exp(-1.34337)*c(1, 1+(main_results$ub[c(2,1,6)])/100))
-est_transmission/sum(est_transmission)*100
+est_transmission <- summary_omicron$count*(exp(model[1,]$beta)*c(1, main_results$ub))
+ub <- est_transmission/sum(est_transmission)*100
 
+
+format_res <- function(x1, x2, x3) {
+  paste0(x1, " (", x2, ", ", x3, ")")
+}
+secondary_results <- round(100*(transmission_frac %>% select(estimate, conf.low, conf.high)),1) 
+tbl <- cbind(labels$label%>%as.character, format_res(secondary_results[,1], secondary_results[,2], secondary_results[,3]),
+             format_res(round(est,1), round(lb,1), round(ub,1)))
+
+
+# keeping only those with complete infection record
 summary_housing <- read_csv("housing_duration.csv")
 omicron_full_data <- omicron %>% left_join(summary_housing)
 summary_omicron <- omicron_full_data %>% 
   filter(first <= "2020-03-31") %>% 
   mutate(prior_inf = ifelse(num_pos>1, 1, 0), 
          prior_vacc = ifelse(num_dose_adjusted>0, 1, 0)) %>% 
-  group_by(prior_vacc, prior_inf) %>% summarise(count=n())
-est_transmission <- summary_omicron$count * (exp(-1.34337)*c(1, 1+(main_results$est[c(2,1,6)])/100))
-est_transmission/sum(est_transmission)*100
+  group_by(prior_inf, prior_vacc) %>% summarise(count=n())
 
+est_transmission <- summary_omicron$count*(exp(model[1,]$beta)*c(1, main_results$risk_red))
+est <- est_transmission/sum(est_transmission)*100
 
-est_transmission <- summary_omicron$count * (exp(-1.34337)*c(1, 1+(main_results$lb[c(2,1,6)])/100))
-est_transmission/sum(est_transmission)*100
+est_transmission <- summary_omicron$count*(exp(model[1,]$beta)*c(1, main_results$lb))
+lb <- est_transmission/sum(est_transmission)*100
 
-est_transmission <- summary_omicron$count * (exp(-1.34337)*c(1, 1+(main_results$ub[c(2,1,6)])/100))
-est_transmission/sum(est_transmission)*100
+est_transmission <- summary_omicron$count*(exp(model[1,]$beta)*c(1, main_results$ub))
+ub <- est_transmission/sum(est_transmission)*100
+
+tbl <- cbind(tbl, format_res(round(est,1), round(lb,1), round(ub,1)))
+tbl
+
+tbl <- tbl %>% as.data.frame()
+names(tbl) <- c("", "Fraction of transmission from SARS-CoV-2 infections (%) (95% CI)\nWithin secondary cases in study population",
+                "Among all Omicron SARS-CoV-2 infections",
+                "Among Omicron SARS-CoV-2 infections with complete prior infection history")
+tbl
+
+write_csv(tbl,"/Users/sophiatan/Documents/UCSF/CDCR-CalProtect/tables/attributable_fraction_s9.csv")
