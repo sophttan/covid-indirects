30627ae95ed10d4f5a82b96104384285c03f473b
Sophia Tan <sophiatan@berkeley.edu>
2022-10-01 12:00:15 -0700

    matching testing

--- 2-analysis/3-alternative matching/alternative matching.R
+++ 2-analysis/3-alternative matching/alternative matching.R
@@ -11,38 +11,144 @@ library(broom)
 library(readr)
 library(MatchIt)
 
-inf_omicron_subset <- read_csv("unmatched_all_covariates.csv")
+inf_omicron_subset <- read_csv("unmatched_all_covariates092922.csv")
 
-# 1:4 matching with 30 day caliper
-day_dist <- 30
+# estimate propensity scores (age, COVID-19 risk, and prior infection history)
+ps <- glm(treatment ~ age + covid_risk + index_prior_inf, data = inf_omicron_subset, family = binomial(link='logit'))
+summary(ps)
+inf_omicron_subset <- inf_omicron_subset %>% mutate(logodds = predict.glm(ps),
+                                                    ps=exp(logodds)/(1+exp(logodds)))
+inf_omicron_subset
+
+# distance matrices for Day and propensity score
+distance_propensity <- dist(as.matrix(inf_omicron_subset%>%select(ps)), diag = T, upper = T) %>% as.matrix()
 distance_matrix <- dist(as.matrix(inf_omicron_subset%>%select(Day)%>%mutate(Day=as.numeric(Day))), diag = T, upper = T) %>% as.matrix()
-m <- matchit(treatment ~ Day + Institution, data = inf_omicron_subset,
-             method = "nearest", exact = "Institution", caliper = c(30),std.caliper = F,
-             distance = distance_matrix, ratio=4, replace = F)
-summary(m)
-m$match.matrix
-mout <- match.data(m)
-final <- mout %>% group_by(subclass) %>% arrange(desc(treatment)) %>% 
-  filter(abs(Day-first(Day))<=day_dist & Institution==first(Institution)) %>% 
-  group_by(subclass) %>% filter(n()>1)
 
-write_csv(final, "matched_data_alternative1_4_30.csv")
-
-
-
-# 1:10 matching with 15 caliper
-day_dist <- 15
-distance_matrix <- dist(as.matrix(inf_omicron_subset%>%select(Day)%>%mutate(Day=as.numeric(Day))), diag = T, upper = T) %>% as.matrix()
-m <- matchit(treatment ~ Day + Institution, data = inf_omicron_subset,
-             method = "nearest", exact = "Institution", caliper = c(15),std.caliper = F,
-             distance = distance_matrix, ratio=10, replace = F)
-summary(m)
-m$match.matrix
-mout <- match.data(m)
-final <- mout %>% group_by(subclass) %>% arrange(desc(treatment)) %>% 
-  filter(abs(Day-first(Day))<=day_dist & Institution==first(Institution)) %>% group_by(subclass) %>% filter(n()>1)
-
-write_csv(final, "matched_data_alternative1_10_15.csv")
-
-
+match <- function(propensity, day, caliper_propensity, caliper_day_dist,
+                  weight_p_d, k, include_propensity=T) {
+  propensity[propensity > caliper_propensity] <- Inf
+  day[day > caliper_day_dist] <- Inf
   
+  if(include_propensity==F) {
+    distance <- day
+    print(distance[1:10,1:10])
+    m <- matchit(treatment ~ Day, data = inf_omicron_subset,
+                 method = "nearest", 
+                 exact = ~Institution,
+                 distance = distance, ratio=k, replace = F)
+  }else{
+    day <- day/caliper_day_dist*caliper_propensity
+    distance <- propensity*weight_p_d[1] + day*weight_p_d[2]
+    print(distance[1:10,1:10])
+    m <- matchit(treatment ~ age + covid_risk + index_prior_inf + Day, data = inf_omicron_subset,
+                 method = "nearest", 
+                 exact = ~Institution,
+                 distance = distance, ratio=k, replace = F)
+    print(plot(m, type = "qq", interactive = FALSE, which.xs = c("age", "covid_risk", "index_prior_inf", "Day")))
+  }
+  
+  print(summary(m))
+  match.data(m)
+}
+
+reweigh_matches <- function(data, caliper_propensity, caliper_day_dist){
+  data <- data %>% group_by(subclass) %>% arrange(desc(treatment)) %>%
+    filter(abs(first(Day)-Day)<=caliper_day_dist&abs(first(ps)-ps)<=caliper_propensity)
+  # reweigh matches
+  data <- data %>% mutate(weights=ifelse(treatment==1,1,1/sum(treatment==0)))
+  mean_weights <- mean((data %>% filter(treatment==0))$weights)
+  data <- data %>% mutate(weights=ifelse(treatment==1,1,weights/mean_weights))
+}
+
+# caliper testing
+# manually set calipers for Day and propensity score
+# propensity score caliper = 0.2
+ps_dist <- 0.2
+day_dist <- 30
+
+matched <- match(distance_propensity, distance_matrix, ps_dist, day_dist, c(0.5, 0.5), 10, T)
+matched %>% group_by(subclass) %>% arrange(subclass, desc(treatment)) %>% 
+  select(subclass, treatment, ps, Day) %>% 
+  filter(treatment==1|(abs(first(Day)-Day)>day_dist|abs(first(ps)-ps)>ps_dist)) %>% filter(n()>1)
+matched <- reweigh_matches(matched, ps_dist, day_dist)
+matched %>% write_csv("matched_data_alternative_cal_propensity.csv")
+
+# day caliper = 15
+ps_dist <- 0.1
+day_dist <- 15
+matched <- match(distance_propensity, distance_matrix, ps_dist, day_dist, c(0.5, 0.5), 10, T)
+matched %>% group_by(subclass) %>% arrange(subclass, desc(treatment)) %>% 
+  select(subclass, treatment, ps, Day) %>% 
+  filter(treatment==1|(abs(first(Day)-Day)>day_dist|abs(first(ps)-ps)>ps_dist)) %>% filter(n()>1)
+matched <- reweigh_matches(matched, ps_dist, day_dist)
+matched %>% group_by(subclass) %>% filter(n()==1)
+matched %>% write_csv("matched_data_alternative_cal_days.csv")
+
+# 1:4 matching 
+ps_dist <- 0.1
+day_dist <- 30
+matched <- match(distance_propensity, distance_matrix, ps_dist, day_dist, c(0.5, 0.5), 4, T)
+matched %>% group_by(subclass) %>% arrange(subclass, desc(treatment)) %>% 
+  select(subclass, treatment, ps, Day) %>% 
+  filter(treatment==1|(abs(first(Day)-Day)>day_dist|abs(first(ps)-ps)>ps_dist)) %>% filter(n()>1)
+matched <- reweigh_matches(matched, ps_dist, day_dist)
+matched %>% group_by(subclass) %>% filter(n()==1)
+matched %>% write_csv("matched_data_alternative1_4ratio.csv")
+
+# changing weights
+ps_dist <- 0.1
+day_dist <- 30
+weight <- c(0.25, 0.75)
+matched <- match(distance_propensity, distance_matrix, ps_dist, day_dist, weight, 10, T)
+matched %>% group_by(subclass) %>% arrange(subclass, desc(treatment)) %>% 
+  select(subclass, treatment, ps, Day) %>% 
+  filter(treatment==1|(abs(first(Day)-Day)>day_dist|abs(first(ps)-ps)>ps_dist)) %>% filter(n()>1)
+matched <- reweigh_matches(matched, ps_dist, day_dist)
+matched %>% group_by(subclass) %>% filter(n()==1)
+matched %>% write_csv("matched_data_alternative_weights_1p_3d.csv")
+
+weight <- c(0.75, 0.25)
+matched <- match(distance_propensity, distance_matrix, ps_dist, day_dist, weight, 10, T)
+matched %>% group_by(subclass) %>% arrange(subclass, desc(treatment)) %>% 
+  select(subclass, treatment, ps, Day) %>% 
+  filter(treatment==1|(abs(first(Day)-Day)>day_dist|abs(first(ps)-ps)>ps_dist)) %>% filter(n()>1)
+matched <- reweigh_matches(matched, ps_dist, day_dist)
+matched %>% group_by(subclass) %>% filter(n()==1)
+matched %>% write_csv("matched_data_alternative_weights_3p_1d.csv")
+
+
+# no propensity score
+matched <- match(distance_propensity, distance_matrix, NULL, day_dist, NULL, 10, F)
+matched %>% group_by(subclass) %>% arrange(subclass, desc(treatment)) %>% 
+  select(subclass, treatment, ps, Day) %>% 
+  filter(treatment==1|(abs(first(Day)-Day)>day_dist)) %>% filter(n()>1)
+matched %>% write_csv("matched_data_alternative_nopropensity.csv")
+
+
+# additional factors in propensity score
+# estimate propensity scores (age, COVID-19 risk, and prior infection history)
+ps_model <- glm(treatment ~ age + covid_risk + index_prior_inf + Race + Sex, data = inf_omicron_subset, family = binomial(link='logit'))
+summary(ps_model)
+inf_omicron_subset <- inf_omicron_subset %>% mutate(logodds = predict.glm(ps_model),ps=exp(logodds)/(1+exp(logodds)))
+
+# distance matrices for Day and propensity score
+distance_propensity <- dist(as.matrix(inf_omicron_subset%>%select(ps)), diag = T, upper = T) %>% as.matrix()
+distance_matrix <- dist(as.matrix(inf_omicron_subset%>%select(Day)%>%mutate(Day=as.numeric(Day))), diag = T, upper = T) %>% as.matrix()
+
+ps_dist <- 0.1
+day_dist <- 30
+distance_propensity[distance_propensity > ps_dist] <- Inf
+distance_matrix[distance_matrix > day_dist] <- Inf
+distance_matrix <- distance_matrix/day_dist*ps_dist
+weights <- c(0.5, 0.5)
+totaldistance <- distance_matrix*weights[1] + distance_propensity*weights[2]
+
+m <- matchit(treatment ~ age + covid_risk + index_prior_inf + Day + Sex + Race, data = inf_omicron_subset,
+             method = "nearest", 
+             exact = ~Institution,
+             distance = totaldistance, ratio=10, replace = F)
+mout <- match.data(m)
+mout %>% group_by(subclass) %>% arrange(subclass, desc(treatment)) %>% 
+  select(subclass, treatment, ps, Day) %>% 
+  filter(treatment==1|(abs(first(Day)-Day)>day_dist)) %>% filter(n()>1)
+mout %>% write_csv("matched_data_alternative_propensity_sex_race.csv")
--- 2-analysis/3-alternative matching/alternative_matching_regression.R
+++ 2-analysis/3-alternative matching/alternative_matching_regression.R
@@ -23,46 +23,39 @@ make_res <- function(model, group, names=NULL) {
              ub=100*(exp(model$beta+2*model$SE)-1))
 }
 
-
-# 1:4 matched results
-matched_1_4 <- read_csv("matched_data_alternative1_4_30.csv")
-
-matched_1_4 <- matched_1_4 %>% mutate(Institution=as.factor(Institution),
+run_regression <- function(data) {
+  data <- data %>% mutate(Institution=as.factor(Institution),
                           index_id=as.factor(index_id))
-model <- glm(contact_status ~ index_prior_vacc + index_prior_inf +
-               num_vacc_doses+has_prior_inf+incidence_log+Institution, 
-             data=matched_1_4, weights=weights, family="poisson")
-summary(model)
+  model <- glm(contact_status ~ index_prior_vacc + index_prior_inf + num_days_in_contact + 
+                 num_vacc_doses+has_prior_inf+incidence_log+Institution, 
+               data=data, weights=weights, family="poisson")
+  print(summary(model))
+  
+  model <- coef_test(model, vcov = "CR2", cluster = data$subclass) %>% as.data.frame()
+  
+  model[2:3,]
+}
 
-model <- coef_test(model, vcov = "CR2", cluster = matched_1_4$subclass) %>% as.data.frame()
-
-model <- model[2:3,]
-res_1_4_30 <- make_res(model, "1:4 matching of index cases by institution and within 30 days") 
-
-
-
-# 1:10 matched results
-matched_1_10 <- read_csv("matched_data_alternative1_10_15.csv")
-
-matched_1_10 <- matched_1_10 %>% mutate(Institution=as.factor(Institution),
-                                      index_id=as.factor(index_id))
-model <- glm(contact_status ~ index_prior_vacc + index_prior_inf +
-               num_vacc_doses+has_prior_inf+incidence_log+Institution, 
-             data=matched_1_10, weights=weights, family="poisson")
-summary(model)
-
-model <- coef_test(model, vcov = "CR2", cluster = matched_1_10$subclass) %>% as.data.frame()
-
-model <- model[2:3,]
-res_1_10_15 <- make_res(model, "1:10 matching of index cases by institution and within 15 days") 
-
+res <- make_res(run_regression(read_csv("matched_data_alternative_cal_propensity.csv")), "Caliper of propensity score = 0.1") 
+res <- rbind(res, 
+             make_res(run_regression(read_csv("matched_data_alternative_cal_days.csv")), "Caliper of days between index cases = 15") )
+res <- rbind(res, 
+             make_res(run_regression(read_csv("matched_data_alternative1_4ratio.csv")), "1:4 matching") )
+res <- rbind(res, 
+             make_res(run_regression(read_csv("matched_data_alternative_weights_1p_3d.csv")), "1:3 ratio") )
+res <- rbind(res, 
+             make_res(run_regression(read_csv("matched_data_alternative_weights_3p_1d.csv")), "3:1 ratio") )
+res <- rbind(res, 
+             make_res(run_regression(read_csv("matched_data_alternative_nopropensity.csv")), "Matching without propensity score") )
+res <- rbind(res, 
+             make_res(run_regression(read_csv("matched_data_alternative_propensity_sex_race.csv")), "Matching with propensity score with sex and race") )
 
 
 # no matching
-nomatch <- read_csv("unmatched_all_covariates.csv")
+nomatch <- read_csv("unmatched_all_covariates092922.csv")
 nomatch <- nomatch %>% mutate(Institution=as.factor(Institution),
                                        index_id=as.factor(index_id))
-model <- glm(contact_status ~ index_prior_vacc + index_prior_inf +
+model <- glm(contact_status ~ index_prior_vacc + index_prior_inf + num_days_in_contact+
                num_vacc_doses+has_prior_inf+incidence_log+Institution, data=nomatch, family="poisson")
 summary(model)
 
@@ -74,12 +67,18 @@ res_nomatch <- data.frame(group="No matching",
                           lb=100*(exp(model[,1]-2*model[,2])-1),
                           ub=100*(exp(model[,1]+2*model[,2])-1), row.names=NULL)
 
-res <- rbind(res_1_10_15, res_1_4_30, res_nomatch) %>% 
-  mutate(covariate=rep(c("Prior vaccination", "Prior infection"), 3)) %>% 
+res <- rbind(res,res_nomatch) %>% 
+  mutate(covariate=rep(c("Prior vaccination", "Prior infection"), 8)) %>% 
   select(1,5,2,3,4)
 
 res_tbl <- res %>% mutate(res = paste0(round(relrisk, 1), " (", round(lb, 1), ", ", round(ub, 1), ")"))
-res_tbl <- res_tbl %>% select(!c(3,4,5))
+res_tbl <- res_tbl %>% mutate(header=c("Varying choice of caliper","","","",
+                                      "Varying k in 1:k matching","",
+                                      "Varying weights between propensity score and time","","","",
+                                      "Overall changes to propensity score","","","",
+                                      "No matching",""))
+res_tbl <- res_tbl %>% select(!c(3,4,5)) %>% select(4,1:3)
 names(res_tbl) <- c("Matching specification", "Index case", "Relative % change in attack rate of infection in close contact (95% CI)")
+
 
 write_csv(res_tbl, "/Users/sophiatan/Documents/UCSF/CDCR-CalProtect/tables/alternative_matching_specs_s6.csv")
