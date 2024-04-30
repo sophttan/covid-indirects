7863e01f387d92b59412a8ed87706b785f0aef18
Sophia Tan <sophiatan@berkeley.edu>
2024-04-30 14:10:01 -0700

    resize

--- case-control-figure.R
+++ case-control-figure.R
@@ -43,7 +43,7 @@ p1 <- ggplot(results%>%filter(group!="By time"), aes(x=time, y=point, color=inf_
         strip.background = element_rect(fill=NA,colour="black"),
         text=element_text(size=12, family="sans")) 
 p1
-ggsave("D:/CCHCS_premium/st/covid-indirects/figures/figure2.jpg", width=10, height=4, dpi=300)
+ggsave("D:/CCHCS_premium/st/covid-indirects/figures/figure2.jpg", width=10.5, height=4, dpi=300)
 
 p2 <- ggplot(results%>%filter(group=="By time"&inf_vacc!="First three months of vaccination")%>%
                mutate(inf_vacc=factor(inf_vacc, labels=c("Vaccine-derived immunity", "Infection-acquired immunity", "Most recent vaccination or infection"))), 
