58b6aab79c8425220eaf3c23d7e53a332c837607
Sophia Tan <sophiatan@berkeley.edu>
2024-11-25 15:03:59 -0800

    update table code

--- 3-figures-tables/table-code.R
+++ 3-figures-tables/table-code.R
@@ -17,12 +17,29 @@ make_tbl <- function(res, names, include_age_risk=T) {
   res %>% select(1, ncol(.)) %>% rename(" "=x)
 }
 
+# results grouped
+make_sensitivity <- function(name, filepath) {
+  res <- read_csv(here::here(paste0(filepath,"binary-results.csv")))[1:2,] %>% 
+    rbind(read_csv(here::here(paste0(filepath,"dose-results.csv")))[1:4,]) %>%
+    rbind(read_csv(here::here(paste0(filepath,"time-results.csv")))%>%filter(grepl("time.+roommate",x)&inf_vacc%in%c("inf","vacc"))%>%select(!inf_vacc)) 
+  tbl <- make_tbl(res, names=c(rep(c("Vaccine-derived immunity Any", "Infection-acquired immunity Any")),
+                               rep(c("By dose Partial vaccination",
+                                     "Primary series alone",
+                                     "One booster",
+                                     "Two or more boosters")),
+                               rep(c("By time <3 months", "3 to <6 months", "6 to <12 months", "12+ months"), 2)), include_age_risk = F)
+  tbl <- tbl %>% mutate(inf=c(c("vacc", "inf"),rep("vacc",4),rep("inf", 4), rep("vacc", 4))) %>% arrange(desc(inf))
+  tbl <- tbl %>% select(!inf)
+  names(tbl) <- c("", name)
+  tbl
+}
 
 # binary results
 res <- read_csv(here::here("results/main/binary-results.csv"))
 res                 
 
 make_tbl(res, c("Vaccine-derived immunity (any)", "Infection-acquired immunity (any)")) %>% write_csv("tables/binary.csv")
+
 
 # dose results
 names <- c("Vaccine-derived immunity (by dose) Partial vaccination",
@@ -62,6 +79,7 @@ tbl <- vacc %>% cbind(inf) %>% cbind(infvacc)
 names(tbl) <- paste(c("","Vaccine", "Infection", "Vaccine or infection"), names(tbl))
 tbl %>% write_csv("tables/time.csv")
 
+
 # waning adjustment for both
 res <- read_csv(here::here("results/main/time-bothinfvacc-results.csv"))
 res                 
@@ -69,6 +87,7 @@ res
 column <- c("Vaccine-derived immunity (time) <3 months", "3 to <6 months", "6 to <12 months", "12+ months",
             "Infection-acquired immunity (time) <3 months", "3 to <6 months", "6 to <12 months", "12+ months")
 make_tbl(res, column, include_age_risk = F) %>% write_csv("tables/waning_bothinfvacc.csv")
+
 
 # 3 month vaccine
 res <- read_csv(here::here("results/main/3months-results.csv"))
@@ -88,6 +107,7 @@ make_tbl(res, c("Ancestral monovalent vaccine <3 months",
                 "Ancestral monovalent vaccine 3+ months",
                 "Bivalent vaccine <3 months"), F) %>% write_csv("tables/bivalent.csv")
 
+
 # interaction
 res <- read_csv(here::here("results/main/interaction-results.csv"))
 res
@@ -102,18 +122,61 @@ tbl$inf <- c("Any infection",
 tbl <- tbl %>% pivot_wider(id_cols=` `, names_from = "inf", values_from = `Odds ratio (OR) (95% CI)`)
 tbl %>% write_csv("tables/interaction.csv")
 
-# binary results
-res <- read_csv(here::here("results/main/stratified-casecontrolimmunity-results.csv"))
-res                 
 
-cbind("Immunity of matched case and controls" = rep(c("No immunity",
-                                                      "Only vaccine-derived immunity",
-                                                      "Only infection-acquired immunity",
-                                                      "Both vaccine- and infection-acquired immunity (hybrid)"), each=2),
-  make_tbl(res, rep(c("Vaccine-derived immunity (any)", "Infection-acquired immunity (any)"), 4), include_age_risk = F)) %>%
-  write_csv("tables/stratified-casecontrol.csv")
+# pre-omicron v omicron
+res <- read_csv(here::here("results/main/preomicron-omicron-results.csv"))[1:8,]
+tbl <- make_tbl(res, rep(c("<3 months", "3 to <6 months", "6 to <12 months", "12+ months"), 2), 
+                include_age_risk = F)
+tbl$inf <- rep(c("pre-Omicron infection", "Omicron infection"), each=4)
+tbl <- tbl %>% pivot_wider(id_cols=` `, names_from = "inf", values_from = `Odds ratio (OR) (95% CI)`)
+tbl %>% write_csv("tables/preomicron-omicron.csv")
 
 
+# stratified results
+res <- read_csv(here::here("results/main/stratified-casecontrolimmunity-results.csv")) %>% 
+  rbind(read_csv(here::here("results/main/stratified-casecontrolimmunity-results-dose.csv")) %>% mutate(n=NA)) %>%
+  rbind(read_csv(here::here("results/main/stratified-casecontrolimmunity-results-vacc.csv"))) %>%
+  rbind(read_csv(here::here("results/main/stratified-casecontrolimmunity-results-inf.csv"))) 
+tbl <- make_tbl(res, names=c(rep(c("Vaccine-derived immunity Any", "Infection-acquired immunity Any"), 4),
+                             rep(c("By dose Partial vaccination",
+                                   "Primary series alone",
+                                   "One booster",
+                                   "Two or more boosters"), 4),
+                             rep(c("By time <3 months", "3 to <6 months", "6 to <12 months", "12+ months"), 8)), include_age_risk = F)
+tbl <- tbl %>% mutate(label=res$label, inf=c(rep(c("vacc", "inf"), 4),
+                                             rep("vacc", 32), rep("inf", 16))) %>% arrange(desc(inf))
+tbl <- tbl %>% pivot_wider(id_cols=c(` `, "inf"), names_from = "label", values_from = `Odds ratio (OR) (95% CI)`)
+tbl <- tbl %>% select(!inf)
+tbl <- cbind(tbl[,1], make_sensitivity("Main", "results/main/")[,2], tbl[,2:5])
+
+names(tbl) <- c("", "All cases and controls", "No immunity","Only vaccine-derived immunity","Only infection-acquired immunity","Both vaccine- and infection-acquired immunity (hybrid)")
+tbl %>% write_csv("tables/stratified-casecontrol.csv")
+
+
+# housing sensitivity results grouped
+tbl <- cbind(make_sensitivity("Days 3-6 (Main)", "results/main/"), 
+             make_sensitivity("Day 3", "results/roommate-3day/")[,2], 
+             make_sensitivity("Days 0-6", "results/roommate-0-6day/")[,2], 
+             make_sensitivity("Days 6-9", "results/roommate-6-9day/")[,2])
+tbl %>% write_csv("tables/housing-sensitivity.csv")
+
+
+# matching/adjustment sensitivity results grouped
+tbl <- cbind(make_sensitivity("Main", "results/main/"), 
+             make_sensitivity("1:1 matching", "results/match11//")[,2], 
+             make_sensitivity("Removing matching by time since vaccine and/or infection", "results/no-time-match/")[,2], 
+             make_sensitivity("Adding statistical adjustment for time since vaccine and/or infection of case/control", "results/timeadj/")[,2],
+             make_sensitivity("Removing statistical adjustment for age and risk", "results/noadj/")[,2])
+tbl %>% write_csv("tables/match-statistical-sensitivity.csv")
+
+
+# negative control flu results
+res <- read_csv("results/main/flu-results.csv")
+make_tbl(res, 
+         c("Any influenza vaccine (lifetime)",
+           "Influenza vaccine within 2 years",
+           "Influenza vaccine within 1 year",
+           "Time since influenza vaccine (months)"), F) %>% write_csv("tables/flu.csv")
 
 
 # full results
@@ -127,10 +190,17 @@ column <- c("Vaccine-derived immunity Any", "By dose Partial vaccination",
             "By time <3 months", "3 to <6 months", "6 to <12 months", "12+ months",
             "Infection-acquired immunity Any",
             "By time <3 months", "3 to <6 months", "6 to <12 months", "12+ months")
-make_tbl(res, column, F) %>% write_csv("tables/logistic.csv")
 
-res <- read_csv("results/logistic/full_results_robust.csv")
-res <- res %>% mutate(name=if_else(grepl("vacc|dose", x), "vacc", "inf")) %>% arrange(desc(name))
-res <- res %>% filter(!grepl("infvacc", x)) %>% select(!name)
-make_tbl(res, column, F) %>% write_csv("tables/logistic_robust.csv")
+res_robust <- read_csv("results/logistic/full_results_robust.csv")
+res_robust <- res_robust %>% mutate(name=if_else(grepl("vacc|dose", x), "vacc", "inf")) %>% arrange(desc(name))
+res_robust <- res_robust %>% filter(!grepl("infvacc", x)) %>% select(!name)
+
+# model sensitivity results grouped
+tbl <- cbind(make_sensitivity("Conditional logistic regression (main)", "results/main/"), 
+             make_tbl(res, column, F)[,2], 
+             make_sensitivity("Conditional logistic regression without repeated residents", "results/no-repeat/")[,2], 
+             make_tbl(res, column, F)[,2])
+names(tbl)[3]<-"Unconditional logistic regression"
+names(tbl)[5]<-"Unconditional logistic regression with person-level clustering"
+tbl %>% write_csv("tables/model-sensitivity.csv")
 
